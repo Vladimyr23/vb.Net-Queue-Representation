d38616544aab96be45e78655b9416119be865ed4
Vladimyr23 <33516849+Vladimyr23@users.noreply.github.com>
2019-09-28 15:10:41 +0100

    Add files via upload

---
+++ QueueRepresentation/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(12, 207)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(110, 207)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 1
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(134, 70)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(178, 106)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 70)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "5 Pound"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 94)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(71, 17)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.Text = "10 Pound"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(12, 117)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(71, 17)
+        Me.RadioButton3.TabIndex = 6
+        Me.RadioButton3.Text = "20 Pound"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(12, 140)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(71, 17)
+        Me.RadioButton4.TabIndex = 7
+        Me.RadioButton4.Text = "50 Pound"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(318, 70)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(178, 106)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 12
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(502, 70)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(178, 106)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 13
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(686, 70)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(178, 106)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 14
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Location = New System.Drawing.Point(870, 70)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(178, 106)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 15
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1056, 250)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Queue"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+End Class
---
+++ QueueRepresentation/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''Dim myAssembly As System.Reflection.Assembly = _ System.Reflection.Assembly.GetExecutingAssembly()
+        ''Dim myStream As Stream = _ myAssembly.GetManifestResourceStream("EmbeddingExample.polymer5features.jpg")
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If RadioButton1.Checked Then
+            If PictureBox1.Image Is Nothing Then
+                PictureBox1.Image = My.Resources.polymer5features
+            ElseIf PictureBox2.Image Is Nothing Then
+                PictureBox2.Image = My.Resources.polymer5features
+            ElseIf PictureBox3.Image Is Nothing Then
+                PictureBox3.Image = My.Resources.polymer5features
+            ElseIf PictureBox4.Image Is Nothing Then
+                PictureBox4.Image = My.Resources.polymer5features
+            ElseIf PictureBox5.Image Is Nothing Then
+                PictureBox5.Image = My.Resources.polymer5features
+            Else
+                MessageBox.Show("The queue is full", "5 Elements Limit",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            End If
+        ElseIf RadioButton2.Checked Then
+            If PictureBox1.Image Is Nothing Then
+                PictureBox1.Image = My.Resources._10
+            ElseIf PictureBox2.Image Is Nothing Then
+                PictureBox2.Image = My.Resources._10
+            ElseIf PictureBox3.Image Is Nothing Then
+                PictureBox3.Image = My.Resources._10
+            ElseIf PictureBox4.Image Is Nothing Then
+                PictureBox4.Image = My.Resources._10
+            ElseIf PictureBox5.Image Is Nothing Then
+                PictureBox5.Image = My.Resources._10
+            Else
+                MessageBox.Show("The queue is full", "5 Elements Limit",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            End If
+        ElseIf RadioButton3.Checked Then
+            If PictureBox1.Image Is Nothing Then
+                PictureBox1.Image = My.Resources._20
+            ElseIf PictureBox2.Image Is Nothing Then
+                PictureBox2.Image = My.Resources._20
+            ElseIf PictureBox3.Image Is Nothing Then
+                PictureBox3.Image = My.Resources._20
+            ElseIf PictureBox4.Image Is Nothing Then
+                PictureBox4.Image = My.Resources._20
+            ElseIf PictureBox5.Image Is Nothing Then
+                PictureBox5.Image = My.Resources._20
+            Else
+                MessageBox.Show("The queue is full", "5 Elements Limit",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            End If
+        ElseIf RadioButton4.Checked Then
+            If PictureBox1.Image Is Nothing Then
+                PictureBox1.Image = My.Resources._50
+            ElseIf PictureBox2.Image Is Nothing Then
+                PictureBox2.Image = My.Resources._50
+            ElseIf PictureBox3.Image Is Nothing Then
+                PictureBox3.Image = My.Resources._50
+            ElseIf PictureBox4.Image Is Nothing Then
+                PictureBox4.Image = My.Resources._50
+            ElseIf PictureBox5.Image Is Nothing Then
+                PictureBox5.Image = My.Resources._50
+            Else
+                MessageBox.Show("The queue is full", "5 Elements Limit",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+            End If
+
+        End If
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        PictureBox1.Image = PictureBox2.Image
+        PictureBox2.Image = PictureBox3.Image
+        PictureBox3.Image = PictureBox4.Image
+        PictureBox4.Image = PictureBox5.Image
+        PictureBox5.Image = Nothing
+    End Sub
+End Class
